4881bb2b999d657121b1cf0e1b10c4ba7deaada1
Ulises Luque <Ulises Luque>
2016-02-13 18:54:06 +0100

    modificado portfolio

--- practicas/Portfolio/js/script.js
+++ practicas/Portfolio/js/script.js
@@ -5,26 +5,39 @@ $(document).ready(function(){
 	var nav = document.getElementById("nav");
 	var html = document.getElementsByTagName("html")[0];
 	var imagenes = ["img/casasRurales.jpg", "img/gestorTareas.jpg", "img/blog.jpg"];
-	var links = ["http://ulisesluque.github.io/Interfaces-Web/practicas/CasasRurales/","http://ulisesluque.github.io/Interfaces-Web/practicas/blog/"];
+	var links = ["http://ulisesluque.github.io/Interfaces-Web/practicas/CasasRurales/", "http://ulisesluque.github.io/Interfaces-Web/practicas/GestorTareas/","http://ulisesluque.github.io/Interfaces-Web/practicas/blog/"];
 	var titulosSlide = ["Casas rurales", "Gestor de tareas", "Blog"];
 	var i = 0;
 
 
 	$("#contenedor").height($(window).height()- $("#nav").height());
-
-	if($(window).width() > 768) 
-		$("#filtro").height($(window).height()- $("#nav").height());
-	else
-		$("#filtro").height($(window).height());
+	$("#filtro").height($(window).height()- $("#nav").height());
 
 	$(window).resize(function(){
-		$("#filtro").css({
-			"background": "no-repeat url('img/Undo.jpg')",
-			"background-size": "cover",
-			"background-position": "center"
+		$("#contenedor").height($(window).height()- $("#nav").height());
+		$("#filtro").height($(window).height()- $("#nav").height());
+		$("#tituloSlide").width($("#slide").width());
 
-		})
-		$('#filtro video').css("display", "none");
+	});
+
+	$("#aInicio").on("click", function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: 0}, 500);
+	});
+
+	$("#aFormacion").on("click", function(e){
+		e.preventDefault();
+			$("html, body").animate({scrollTop: $("#formacion").offset().top - ($("#nav").height())}, 500);	
+	});
+
+	$("#aContacto").on("click", function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: $("#contacto").offset().top - $("#nav").height()}, 500);
+	});
+
+	$("#portfolio").on("click", function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: $("#portf").offset().top - $("#nav").height()}, 500);
 	});
 	
 
@@ -36,6 +49,8 @@ $(document).ready(function(){
 		}
 	});
 
+	$("#tituloSlide").width($("#slide").width());
+
 	$(flecha1).on("click", function(event){
 		event.preventDefault();
 		event.stopPropagation();
@@ -43,11 +58,15 @@ $(document).ready(function(){
 			i--;
 		else
 			i=imagenes.length-1;
-		$("#slide").css("background", "url('"+imagenes[i]+"')");
-		$("#slide").css("background-size", "cover");
-		$("#slide").css("background-position", "center");
-		$("#linkSlide").attr("href", links[i]);
-		$("#tituloSlide").html(titulosSlide[i]);
+		$("#slide").animate({opacity: 0}, function(){
+			$("#slide").css("background", "url('"+imagenes[i]+"')");
+			$("#slide").css("background-size", "cover");
+			$("#slide").css("background-position", "center");
+			$("#linkSlide").attr("href", links[i]);
+			$("#tituloSlide span").html(titulosSlide[i]);
+			$("#slide").animate({opacity: 1});
+		});
+
 	});
 
 	$(flecha2).on("click", function(event){
@@ -57,16 +76,21 @@ $(document).ready(function(){
 			i++;
 		else
 			i = 0;
-		$("#slide").css("background", "url('"+imagenes[i]+"')");
-		$("#slide").css("background-size", "cover");
-		$("#slide").css("background-position", "center");
-		$("#linkSlide").attr("href", links[i]);
-		$("#tituloSlide").html(titulosSlide[i]);
+		$("#slide").animate({opacity: 0}, function(){
+			$("#slide").css("background", "url('"+imagenes[i]+"')");
+			$("#slide").css("background-size", "cover");
+			$("#slide").css("background-position", "center");
+			$("#linkSlide").attr("href", links[i]);
+			$("#tituloSlide span").html(titulosSlide[i]);
+			$("#slide").animate({opacity: 1});
+		});
+
+
 	});
 
 	$(window).on("scroll", function(){
-		scroll = html.scrollTop;
-		if(scroll  < $(window).height() - nav.offsetHeight){
+		scroll = $("html").scrollTop();
+		if(scroll  < $(window).height() - $("nav").outerHeight()){
 			$("#nav").css("position", "relative");
 			$("#nav").css("top", "0");
 			$("#nav").css("margin", "0 0 0 0")
